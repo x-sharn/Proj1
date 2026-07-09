2b1e3bb6e54ab93da3397d159e08927d7c8ff1db
x-sharn <xsharon.work@gmail.com>
2026-07-10 03:04:48 +0530

    feat: add reusable hero component with props

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,20 @@
 import Navbar from '@/components/Navbar';
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
-      <Navbar name="Sharon Xavier" role="Future Full Stack Developer" location="India"
+      <Navbar
+        name="Sharon Xavier"
+        role="Future Full Stack Developer"
+        company="India"
       />
 
-      <main className="min-h-screen bg-slate-900 flex flex-col items-center justify-center text-white px-6">
-        <h1 className="text-5xl font-bold mb-4">
-          Hi, I'm Sharon Xavier.
-          
-        </h1>
-
-        <p className="text-lg text-gray-300 text-center max-w-xl mb-8">
-        Future Full Stack Developer,
-          I build modern websites using Next.js,
-          React and Tailwind CSS.        </p>
-
-        <button className="bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg font-semibold transition">
-          View Projects
-        </button>
-      </main>
+      <Hero
+        name="Sharon Xavier"
+        role="Future Full Stack Developer"
+        description="I'm learning Next.js step by step and building projects to become a professional Full Stack Developer."
+      />
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+export default function Hero({ name, role, description }) {
+    return (
+      <section className="min-h-screen flex items-center justify-center bg-slate-900 text-white px-6">
+        <div className="text-center max-w-3xl">
+  
+          <p className="text-blue-400 text-lg mb-3">
+          Welcome!
+          </p>
+  
+          <h1 className="text-5xl md:text-7xl font-extrabold mb-4">
+            {name}
+          </h1>
+  
+          <h2 className="text-2xl md:text-3xl text-gray-300 mb-6">
+            {role}
+          </h2>
+  
+          <p className="text-lg text-gray-400 leading-8 mb-10">
+            {description}
+          </p>
+  
+          <div className="flex justify-center gap-4">
+  
+            <button className="bg-green-600 hover:bg-blue-700 px-6 py-3 rounded-lg font-semibold transition">
+              View Projects
+            </button>
+  
+            <button className="border border-white hover:bg-white hover:text-black px-6 py-3 rounded-lg font-semibold transition">
+              Contact Me
+            </button>
+  
+          </div>
+  
+        </div>
+      </section>
+    );
+  }
