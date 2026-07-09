a3a95ba02e63967c72aa0770df1743ce584a54bf
x-sharn <xsharon.work@gmail.com>
2026-07-09 12:07:51 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-white">
+        Welcome to Sharon's Portfolio    
+         </h1>
+    </main>
+  );
+}
