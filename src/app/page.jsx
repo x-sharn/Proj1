a79511b86e0aea27aab3d19f5396d1681f55ed30
x-sharn <xsharon.work@gmail.com>
2026-07-10 02:51:02 +0530

    feat: make navbar reusable using props

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,17 +3,20 @@ import Navbar from '@/components/Navbar';
 export default function Home() {
   return (
     <>
-      <Navbar name="Sharon Xavier" role="Next.js Developer" />
-  
+      <Navbar name="Sharon Xavier" role="Future Full Stack Developer" location="India"
+      />
+
       <main className="min-h-screen bg-slate-900 flex flex-col items-center justify-center text-white px-6">
         <h1 className="text-5xl font-bold mb-4">
-           Portfolio 
+          Hi, I'm Sharon Xavier.
+          
         </h1>
-  
+
         <p className="text-lg text-gray-300 text-center max-w-xl mb-8">
-          Hi! I'm learning Next.js and Tailwind CSS.
-        </p>
-  
+        Future Full Stack Developer,
+          I build modern websites using Next.js,
+          React and Tailwind CSS.        </p>
+
         <button className="bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg font-semibold transition">
           View Projects
         </button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
-export default function Navbar({ name, role }) {
+export default function Navbar({ name, role, location }) {
 
     return (
-        <nav className="bg-balck text-white p-4">
+        <nav className="bg-balck text-white py-5 px-8">
             <div className="container mx-auto flex justify-between items-center">
 
-                <h1 className="text-xl font-bold">
-                    {name}
-                </h1>
-                <p className="text-sm text-gray-300 font-bold">
-                    {role}
-                </p>
+                <div>
+                    <h1 className="text-xl font-bold">
+                        {name}
+                    </h1>
+
+                    <p className="text-sm text-gray-300">
+                        {role}
+                    </p>
+                    <p className="text-sm text-gray-300">
+                        {location}
+                    </p>
+                </div>
 
                 <ul className="flex space-x-4">
-                    <li>Portfolio</li>
+                    <li className="cursor-pointer hover:text-blue-400 transition">
+                        Portfolio
+                    </li>
                     <li>About</li>
                     <li>Projects</li>
                     <li>Skills</li>
